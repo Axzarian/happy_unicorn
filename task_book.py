3dd7d23af072f456d9a16a5edaff8a3047a5057d
Alexey M <baoctos@gmail.com>
2023-01-30 23:22:05 +0600

    Добавил расширение для файла task_book

---
+++ task_book.py
@@ -0,0 +1,23 @@
+""" Задача один """
+
+length = input("Введите длину стороны a=")
+length = int(length)
+P = 4*length
+print (P)
+
+
+a = input("Введите длину стороны квадрата a=")
+a = int(a)
+S = a*a
+print(S)
+
+a = input("Ведите длину прямоугольника a=") 
+b = input("Введите ширину прямоугольника b=")
+a = int(a)
+b = int(b)
+S = a*b 
+P = 2*(a+b)
+print ("Площадь равна")
+print (S)
+print ("Периметр равен")
+print (P)
