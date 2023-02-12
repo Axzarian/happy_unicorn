c985174bf2dacba6862a5b87a614e78aa1eba006
Alexey M <baoctos@gmail.com>
2023-02-12 12:10:24 +0600

    Добавил для домашки нахождение четного и с минусовыми значениями

---
+++ circle_practice/Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int i = 1;
+
+if (N > 0)
+{
+    while (i <= N)
+    {
+        if (i % 2 == 0)
+        
+        {
+            Console.WriteLine(i);
+        }
+    i++;
+    }
+}
+else 
+{
+    while (i > N)
+    {
+        if (i % 2 == 0)
+        {
+            System.Console.WriteLine(i);
+        }
+        i--;
+    }
+}
