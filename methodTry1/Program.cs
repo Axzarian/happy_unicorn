036076513dbba1bc3bfc8b9ff0251bf6daed1eff
Alexey M <baoctos@gmail.com>
2023-02-14 22:57:47 +0600

    Сделал метод сложения самостоятельно

---
+++ methodTry1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int Mult(int num1, int num2, int num3)
+{
+    int result = (num1 * num1) + (num2 * num2) + (num3 * num3);
+    return result;
+}
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int sum = Mult(a, b, c);
+System.Console.WriteLine(sum);
